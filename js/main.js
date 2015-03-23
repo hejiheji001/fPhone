c88477f700d42c349568bf35e23f41d13c1a51d9
hejiheji001@gmail.com <hejiheji001@gmail.com>
2015-03-24 00:44:22 +0800

    battery and clock

--- js/main.js
+++ js/main.js
@@ -1,19 +1,62 @@
+Vue.filter('dateToTime', function (date, isBlink) {
+	var hour = date.getHours();
+	var minu = date.getMinutes();
+
+	hour = hour < 10 ? '0' + hour : hour;
+	minu = minu < 10 ? '0' + minu : minu;
+
+	var blink = date.getSeconds() % 2 + Number.parseInt(isBlink);
+
+	return hour + (blink === 0 ? " " : ":") + minu;
+});
+
+Vue.filter('numberToBattery', function (level) {
+	var percent = (level / 60).toFixed();
+	return percent;
+});
+
 var init = function(){
-	var vm = new Vue({
+	var battery = new Vue({
+		el: '#full',
+		data: {
+			w: 20,
+			c: 'rgb(8, 189, 8)'
+		},
+		methods: {
+			setBattery: function(){
+				this.w = this.w > 1 ? this.w -= 0.1 : 1;
+
+				if(this.w <= 10){
+					this.c = 'rgb(255, 255, 0)';
+				}
+				if(this.w <= 5){
+					this.c = 'rgb(255, 0, 0)';
+				}
+			}
+		}
+	});
+
+	var clock = new Vue({
 		el: '#clock',
 		data: {
-			date: new Date().getTime()
+			date: new Date()
 		},
-		methods:{
+		methods: {
 			getTime: function(){
-				this.date++
+				this.date = new Date();
 			}
 		}
 
 	});
-	setInterval(vm.getTime, 1000);
-}
 
-var dateToTime = function(){
+	var power = self.setInterval(function(){
+		battery.setBattery();
+		if(battery.$data.w === 1){
+			window.clearInterval(power);
+		}
+	}, 1000);
 
-}
+	setInterval(function(){
+		clock.getTime();
+	}, 1000);
+};
